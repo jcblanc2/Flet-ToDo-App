1bd3d59b676c22749558288aa2da2c102fdd7c24
jcblanc2 <jclaytonblanc@gmail.com>
2023-08-21 15:44:16 -0400

    Reusable UI components

--- app.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):     
-    page.theme = ft.theme.Theme(color_scheme_seed='green')    
-    page.add(ft.Container(width=100, height=100, bgcolor=ft.colors.CYAN_800), ft.Text(value="hello"))
-    
-    
-
-ft.app(target=main)   
--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import flet as ft
 
+
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.txt_task = ft.TextField(hint_text="Enter task", expand=True)
+        self.tasks_colunm = ft.Column()
+        self.view_main_colunm = ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.txt_task,
+                        ft.FloatingActionButton(
+                            icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks_colunm,
+            ],
+        )
+
+        return self.view_main_colunm
+
+    def add_clicked(self, e):
+        self.tasks_colunm.controls.append(ft.Checkbox(label=self.txt_task.value))
+        self.txt_task.value = ""
+        self.view_main_colunm.update()
+
+
 def main(page: ft.Page):
-    def add_clicked(e):
-        tasks_view.controls.append(ft.Text(value=txt_task.value))
-        txt_task.value = ""
-        view.update()
-
-    txt_task = ft.TextField(hint_text="Enter new task", expand=True)
-    btn_add = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked)
-    tasks_view = ft.Column()
-    view = ft.Column(
-        width=600,
-        controls=[
-            ft.Row(
-                controls=[
-                    txt_task,
-                    btn_add
-                ],
-            ),
-            tasks_view,
-        ],
-    )
-
+    page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
+
+    todo = TodoApp()
+
+    page.add(todo)
+
 
 ft.app(target=main)
